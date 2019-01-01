288dd0d2b6edf483140b4f92a03b6b0b020fabda
loquence <ryandryv@gmail.com>
2019-01-01 11:16:12 -0500

    gulp sucks ass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function () {
   return gulp.src('./css/*.scss')
@@ -31,6 +33,19 @@ gulp.task('browser-sync', function () {
 });
 
 // Default task
-gulp.task('default', gulp.series('browser-sync', function() {
-    gulp.start('sass:watch');
-}));
+gulp.task('default', gulp.series('browser-sync', 'sass:watch'));
+
+gulp.task('clean', function(){
+   return del(['dist']) 
+});
+
+gulp.task('copyfonts', function(){
+   gulp.src('./node_modules/font-awesom/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}').pipe(imagemin({ optimizationLevel: 3, progressive: true})).pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('copyfonts','imagemin')));
